dad9a2b19d187acd3d093de4531973cb66407adc
Zaquariah Holland <zaquariah@gmail.com>
2021-10-08 16:47:20 -0500

    Updates

---
+++ precache-manifest.706103957db7e9d5467d5b11ed6357b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6aa890558f3396eaace7389fb4a029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af100572cfccd3445aa8",
+    "url": "/static/css/main.29c70635.chunk.css"
+  },
+  {
+    "revision": "ef7fcc7a6485f93ea8fa",
+    "url": "/static/js/2.490b673e.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.490b673e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af100572cfccd3445aa8",
+    "url": "/static/js/main.61e653a8.chunk.js"
+  },
+  {
+    "revision": "e2f1fd7ab1f2a13a3824",
+    "url": "/static/js/runtime-main.9aa7c66d.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "b7a9d36a38139677471141f20ab42b62",
+    "url": "/static/media/feature-acm-wiki.b7a9d36a.png"
+  },
+  {
+    "revision": "975539bb2a036f37aedf6353326413ea",
+    "url": "/static/media/feature-roadrunner-cycling.975539bb.jpg"
+  },
+  {
+    "revision": "2e478bc1f4b8e428d5e548890dd7cb4c",
+    "url": "/static/media/feature-rowdyhacks.2e478bc1.png"
+  },
+  {
+    "revision": "a520cf524e9cb7d10012d64916ec5e01",
+    "url": "/static/media/feature-tile-icon-01.a520cf52.svg"
+  },
+  {
+    "revision": "5111bcd5cbd9a25e7c2e8c017eeb3eb2",
+    "url": "/static/media/feature-tile-icon-02.5111bcd5.svg"
+  },
+  {
+    "revision": "198608f5e4889b441864d8be6843cd09",
+    "url": "/static/media/feature-tile-icon-03.198608f5.svg"
+  },
+  {
+    "revision": "dc69deddc05b57617f990411d930243f",
+    "url": "/static/media/feature-tile-icon-04.dc69dedd.svg"
+  },
+  {
+    "revision": "af818383a38fe7a06acead98d9d17d72",
+    "url": "/static/media/feature-tile-icon-06.af818383.svg"
+  },
+  {
+    "revision": "2b0c37c3fa22e3ce5c531c65c5aa0e7e",
+    "url": "/static/media/illustration-section-01.2b0c37c3.svg"
+  },
+  {
+    "revision": "7593fba705de9ae7b533fa9b61633839",
+    "url": "/static/media/illustration-section-02.7593fba7.svg"
+  },
+  {
+    "revision": "8a9d1399adc77b5079704672152ef497",
+    "url": "/static/media/video-placeholder.8a9d1399.png"
+  },
+  {
+    "revision": "4359350e29ca89b8c812351a437d7bd1",
+    "url": "/static/media/z-logo.4359350e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d1264af5285c557a3b4e335961b3e627.js"
+  "/precache-manifest.706103957db7e9d5467d5b11ed6357b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
